ce0d6c3be30c55e53d74b87801c112ecf8f01687
Khaled Taha <khaledtaha105@gmail.com>
2023-03-06 01:18:15 +0200

    Update the Redis Config

--- product/src/main/java/com/kam/product/services/impl/ProductServiceImpl.java
+++ product/src/main/java/com/kam/product/services/impl/ProductServiceImpl.java
@@ -5,27 +5,21 @@ import com.kam.product.models.Product;
 import com.kam.product.repositories.ProductRepository;
 import com.kam.product.services.ProductService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
 import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
-
-    @Autowired
-    private RedisTemplate<String, List<Product>> redisTemplate;
 
 
     @Cacheable(value = "products", key = "#root.methodName")
@@ -35,20 +29,10 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
+    @Cacheable(cacheNames = "productsCache", key = "'products-' + #page.toString() + '-' + #size.toString()")
     public List<Product> getPaginatedProducts(int page, int size) {
-        String cacheKey = "products-" + page + "-" + size;
-        List<Product> cachedProducts = redisTemplate.opsForValue().get(cacheKey);
-
-        if (cachedProducts != null) {
-            return cachedProducts;
-        }
-
         Pageable pageable = PageRequest.of(page, size);
-        List<Product> products = productRepository.findAll(pageable).getContent();
-
-        redisTemplate.opsForValue().set(cacheKey, products);
-
-        return products;
+        return productRepository.findAll(pageable).getContent();
     }
 
 
