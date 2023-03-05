f269a232cdda918b827b6e7df5fb0f5b2b024cd1
Khaled El-Attar <61011535+khaled-taha@users.noreply.github.com>
2023-03-05 22:37:34 +0200

    Update ProductServiceImpl.java

--- product/src/main/java/com/kam/product/services/impl/ProductServiceImpl.java
+++ product/src/main/java/com/kam/product/services/impl/ProductServiceImpl.java
@@ -29,7 +29,6 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    @Cacheable(value = "paginatedProducts", key = "#root.methodName")
     public List<Product> getPaginatedProducts(int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         return productRepository.findAll(pageable).getContent();
